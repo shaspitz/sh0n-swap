eafe4734113618d8fe6a574c6c7b65a466fd8060
Shawn Marshall-Spitzbart <smarshall-spitzbart@berkeley.edu>
2022-01-30 01:37:17 -0800

    init supply

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,8 @@ const Sh0nToken = artifacts.require("Sh0nToken");
 const EthSwapper = artifacts.require("EthSwapper");
 
 module.exports = function(deployer) {
-  // Deploy ERC20 contract for Sh0nToken.
-  deployer.deploy(Sh0nToken); 
+  // Deploy ERC20 contract for Sh0nToken with init supply of 100.
+  deployer.deploy(Sh0nToken, 100); 
 
   // Deploy ETH swapper contract. 
   deployer.deploy(EthSwapper);
