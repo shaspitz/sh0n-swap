8eb6ed2d663c049c373a3b8779a1d85b85419f99
Shawn Marshall-Spitzbart <smarshall-spitzbart@berkeley.edu>
2022-01-29 22:53:01 -0800

    cleanz

--- src/components/App.js
+++ src/components/App.js
@@ -9,11 +9,10 @@ class App extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href="https://github.com/smarshall-spitzbart"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
           </a>
         </nav>
         <div className="container-fluid mt-5">
@@ -21,23 +20,23 @@ class App extends Component {
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
                 <a
-                  href="http://www.dappuniversity.com/bootcamp"
+                  href="https://github.com/smarshall-spitzbart"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
                   <img src={logo} className="App-logo" alt="logo" />
                 </a>
-                <h1>Dapp University Starter Kit</h1>
+                <h1>first proj</h1>
                 <p>
                   Edit <code>src/components/App.js</code> and save to reload.
                 </p>
                 <a
                   className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
+                  href="https://github.com/smarshall-spitzbart"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
+                  first proj <u><b></b></u>
                 </a>
               </div>
             </main>
