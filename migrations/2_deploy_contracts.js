d499b56c3187ea9d52cc0399c965c077964175a3
Shawn Marshall-Spitzbart <smarshall-spitzbart@berkeley.edu>
2022-01-30 13:58:42 -0800

    give tokens to contract on migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,16 @@
 const Sh0nToken = artifacts.require("Sh0nToken");
 const EthSwapper = artifacts.require("EthSwapper");
 
-module.exports = function(deployer) {
-  // Deploy ERC20 contract for Sh0nToken with init supply of 100.
-  deployer.deploy(Sh0nToken, 100); 
+module.exports = async function(deployer) {
+    // Deploy ERC20 contract for Sh0nToken with init supply of 100.
+    await deployer.deploy(Sh0nToken, 100); 
+    const token = await Sh0nToken.deployed();
 
-  // Deploy ETH swapper contract. 
-  deployer.deploy(EthSwapper);
+    // Deploy ETH swapper contract. 
+    await deployer.deploy(EthSwapper);
+    const ethSwapper = await EthSwapper.deployed();
+
+    // Transfer all tokens to EthSwapper contract. 
+    const totalSupply = await token.totalSupply();
+    await token.transfer(ethSwapper.address, totalSupply);
 };
