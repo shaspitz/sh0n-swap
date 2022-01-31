2a73e979bc530763498498eda69ed71913389b23
Shawn Marshall-Spitzbart <smarshall-spitzbart@berkeley.edu>
2022-01-30 18:08:44 -0800

    refactors

--- test/EthSwapper.test.js
+++ test/EthSwapper.test.js
@@ -1,20 +1,25 @@
-const { assert } = require("chai")
 
-const Sh0nToken = artifacts.require("Sh0nToken", 100)
+const Sh0nToken = artifacts.require("Sh0nToken")
 const EthSwapper = artifacts.require("EthSwapper")
 
 require("chai")
     .use(require("chai-as-promised"))
     .should()
 
+// Works for any token with 18 decimals, similar to ETH. 
+function tokensToSmallestDecimal(numTokens) {
+    return web3.utils.toWei(numTokens, "ether");
+}
+
 contract("EthSwapper", (accounts) => {
     let sh0nToken, ethSwapper
 
+    // Deploy contracts before tests. 
     before(async () => {
-        sh0nToken = await Sh0nToken.new(100)
+        sh0nToken = await Sh0nToken.new(tokensToSmallestDecimal("100"))
         ethSwapper = await EthSwapper.new()
         // Transfer all tokens to Eth Swapper contract.
-        await sh0nToken.transfer(ethSwapper.address, 100)
+        await sh0nToken.transfer(ethSwapper.address, tokensToSmallestDecimal("100"))
     })
 
     describe("Sh0nToken deployment", async () => {
@@ -25,7 +30,7 @@ contract("EthSwapper", (accounts) => {
  
         it("Token has initial supply as specified", async() => {
             const initSupply = await sh0nToken.totalSupply()
-            assert.equal(initSupply, 100)
+            assert.equal(initSupply, tokensToSmallestDecimal("100"))
         })
     })
 
@@ -37,7 +42,7 @@ contract("EthSwapper", (accounts) => {
 
         it("Eth swapper contract has all the tokens", async () => {
             const balance = await sh0nToken.balanceOf(ethSwapper.address)
-            assert.equal(balance, 100)
+            assert.equal(balance, tokensToSmallestDecimal("100"))
         })
     })
 })
