99c8646fd8253f2325f86a2375b612b90bc2fe37
Shawn Marshall-Spitzbart <smarshall-spitzbart@berkeley.edu>
2022-02-07 18:16:59 -0800

    preview token conversion

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,14 @@ import ethLogo from '../ethLogo.png'
 
 class Main extends Component {
 
+  constructor(props) {
+    super(props)
+    // Default state.
+    this.state = {
+        output: '0',
+    }
+  }
+
   render() {
     return (
     <div id="content">
@@ -19,55 +27,56 @@ class Main extends Component {
             this.props.buyTokens(etherAmount)
             }}>
             <div>
-            <label className="float-left"><b>Input</b></label>
-            <span className="float-right text-muted">
+              <label className="float-left"><b>Input</b></label>
+              <span className="float-right text-muted">
                 Balance: {ethers.utils.formatEther(this.props.ethBalance)}
-            </span>
+              </span>
             </div>
             <div className="input-group mb-4">
-            <input
+              <input
                 type="text"
                 onChange={(event) => {
-                const etherAmount = this.input.value.toString()
-                this.setState({
-                    output: etherAmount * 100
-                })
+                  const etherAmount = this.input.value.toString();
+                  this.setState({
+                  output: etherAmount * 100
+                  });
                 }}
                 ref={(input) => { this.input = input }}
                 className="form-control form-control-lg"
                 placeholder="0"
                 required />
-            <div className="input-group-append">
+              <div className="input-group-append">
                 <div className="input-group-text">
-                <img src={ethLogo} height='32' width='32' alt=""/>
+                  <img src={ethLogo} height='32' width='32' alt=""/>
                     ETH
                 </div>
-            </div>
+              </div>
             </div>
             <div>
-            <label className="float-left"><b>Output</b></label>
-            <span className="float-right text-muted">
+              <label className="float-left"><b>Output</b></label>
+              <span className="float-right text-muted">
                 Balance: {ethers.utils.formatEther(this.props.sh0nTokenBalance)}
-            </span>
+              </span>
             </div>
             <div className="input-group mb-2">
-            <input
+              <input
                 type="text"
                 className="form-control form-control-lg"
                 placeholder="0"
+                value={this.state.output}
                 disabled
-            />
+              />
             <div className="input-group-append">
-                <div className="input-group-text">
+              <div className="input-group-text">
                 <img src={dino} height='32' width='32' alt=""/>
                     Sh0n
-                </div>
+              </div>
             </div>
             </div>
-            <div className="mb-5">
-            <span className="float-left text-muted">Exchange Rate</span>
-            <span className="float-right text-muted">1 ETH = 100 ShonToken</span>
-            </div>
+              <div className="mb-5">
+                <span className="float-left text-muted">Exchange Rate</span>
+                <span className="float-right text-muted">1 ETH = 100 ShonToken</span>
+              </div>
             <button type="submit" className="btn btn-primary btn-block btn-lg">SWAP!</button>
         </form>
         </div>
